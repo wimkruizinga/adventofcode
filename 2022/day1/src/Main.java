8264b9fbf936b627bac328b2501b59c44c61103b
Wim Kruizinga <kruizinga.w@tkppensioen.nl>
2022-12-02 18:25:40 +0100

    Day 1, part 1

---
+++ 2022/day1/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("data/input.txt"));
+        List<Elf> elves = process(reader);
+
+        for (Elf elf : elves) {
+            System.out.println(elf.getTotalCalories());
+        }
+
+        Elf theBest = Collections.max(elves, Comparator.comparingInt(Elf::getTotalCalories));
+        System.out.printf("Elf number %d carries the most calories: %d", elves.indexOf(theBest) + 1, theBest.getTotalCalories());
+    }
+
+    public static List<Elf> process(BufferedReader reader) throws IOException {
+        List<Elf> elves = new ArrayList<>();
+        Elf elf = new Elf();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isBlank()) {
+                elves.add(elf);
+                elf = new Elf();
+                continue;
+            }
+            elf.addItem(new Item(Integer.parseInt(line)));
+        }
+        return elves;
+    }
+}
