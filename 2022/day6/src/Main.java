c7118fca95fbe88423d7361087ede3da04edd1b0
Wim Kruizinga <kruizinga.w@tkppensioen.nl>
2022-12-11 12:53:45 +0100

    Day 6 part 2

--- 2022/day6/src/Main.java
+++ 2022/day6/src/Main.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-    static final int MARKER_LENGTH = 4;
+    static final int START_OF_PACKET_MARKER_LENGTH = 4;
+    static final int START_OF_MESSAGE_MARKER_LENGTH = 14;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("data/input.txt", StandardCharsets.UTF_8));
@@ -16,22 +17,31 @@ public class Main {
         while ((intChar = reader.read()) != -1) {
             currentPos++;
             signal.append((char) intChar);
-            if (signal.length() < MARKER_LENGTH) {
-                System.out.println(signal.toString() + " is too short");
+            if (signal.length() < START_OF_PACKET_MARKER_LENGTH) {
+                System.out.println(signal + " is too short");
                 continue;
             }
 
-            markerCandidate = signal.substring(currentPos - MARKER_LENGTH, currentPos);
+            markerCandidate = signal.substring(currentPos - START_OF_PACKET_MARKER_LENGTH, currentPos);
             if (isMarker(markerCandidate)) {
-                System.out.printf("%s is a marker. Pos = %d%n", markerCandidate, currentPos);
-                break;
-            } else {
-                System.out.printf("%s is not a marker%n", markerCandidate);
+                System.out.printf("%s is a start of packet marker. Pos = %d%n", markerCandidate, currentPos);
+            }
+
+            if (signal.length() >= START_OF_MESSAGE_MARKER_LENGTH) {
+                String messageMarkerCandidate = signal.substring(currentPos - START_OF_MESSAGE_MARKER_LENGTH, currentPos);
+                if (isStartOfMessage(messageMarkerCandidate)) {
+                    System.out.printf("%s is a start of message marker. Pos = %d%n", messageMarkerCandidate, currentPos);
+                    break;
+                }
             }
         }
     }
 
     private static boolean isMarker(String frame) {
         return frame.chars().distinct().count() == 4;
+    }
+
+    private static boolean isStartOfMessage(String frame) {
+        return frame.chars().distinct().count() == 14;
     }
 }
